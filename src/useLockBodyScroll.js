f73269df1a4a2d6dfc2e1375b5f8d27fc8459527
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2020-01-05 12:31:14 +0100

    Adds locked param to use lock body scroll hook for more flexibility

--- src/useLockBodyScroll.js
+++ src/useLockBodyScroll.js
@@ -1,13 +1,15 @@
 import { useLayoutEffect } from "react";
 
-export function useLockBodyScroll() {
+export function useLockBodyScroll(locked = true) {
   useLayoutEffect(() => {
     const originalStyle = window.getComputedStyle(document.body).overflow;
 
-    document.body.style.overflow = "hidden";
+    if (locked) {
+      document.body.style.overflow = "hidden";
+    }
 
     return () => (document.body.style.overflow = originalStyle);
-  }, []);
+  }, [locked]);
 }
 
 export default useLockBodyScroll;
