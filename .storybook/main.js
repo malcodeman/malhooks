1a598d744686b5a1634017ed8e5624050a9c6b9b
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2020-06-09 12:37:29 +0200

    Adds storybook actions and source

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,9 @@
 module.exports = {
   stories: ["../stories/**/*.stories.js"],
-  addons: ["@storybook/addon-actions", "@storybook/addon-links"],
+  addons: [
+    "@storybook/addon-actions",
+    "@storybook/addon-links",
+    "@storybook/addon-storysource",
+    "@storybook/addon-actions/register",
+  ],
 };
--- stories/useKeyPress.stories.js
+++ stories/useKeyPress.stories.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { action } from "@storybook/addon-actions";
 
 import { useKeyPress } from "../src/useKeyPress";
 
 export default { title: "useKeyPress" };
 
 export function Default() {
-  const pressed = useKeyPress("Escape", () => console.log("ESCAPE PRESSED"));
+  const pressed = useKeyPress("Escape", () => action("ESCAPE PRESSED")());
 
   return (
     <>
